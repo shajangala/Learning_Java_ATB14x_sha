ea0d5977e73f00410c72cbb31268255b1ec44c92
Shamili <sha.jangala@gmail.com>
2025-09-20 08:30:34 +0530

    Java programs

---
+++ src/ex_04_Java_Basics/ninth_table_java_program.java
@@ -0,0 +1,29 @@
+package ex_04_Java_Basics;
+
+public class ninth_table_java_program {
+    public static void main(String[] args) {
+        int i=9;
+        System.out.printf("Printing 9th Table using Java Program");
+        System.out.println();
+        System.out.printf("%d *  1 = %d",i,i*1);
+        System.out.println();
+        System.out.printf("%d *  2 = %d",i,i*2);
+        System.out.println();
+        System.out.printf("%d *  3 = %d",i,i*3);
+        System.out.println();
+        System.out.printf("%d *  4 = %d",i,i*4);
+        System.out.println();
+        System.out.printf("%d *  5 = %d",i,i*5);
+        System.out.println();
+        System.out.printf("%d *  6 = %d",i,i*6);
+        System.out.println();
+        System.out.printf("%d *  7 = %d",i,i*7);
+        System.out.println();
+        System.out.printf("%d *  8 = %d",i,i*8);
+        System.out.println();
+        System.out.printf("%d *  9 = %d",i,i*9);
+        System.out.println();
+        System.out.printf("%d * 10 = %d",i,i*10);
+        System.out.println();
+    }
+}
