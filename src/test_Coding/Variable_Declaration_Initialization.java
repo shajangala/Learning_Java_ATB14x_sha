acb9f49bb32b7ee7e297e6d79b1380fe6b3169d5
Shamili <sha.jangala@gmail.com>
2025-10-12 11:21:49 +0530

    new practice programs

--- src/test_Coding/Variable_Declaration_Initialization.java
+++ src/test_Coding/Variable_Declaration_Initialization.java
@@ -2,13 +2,13 @@ package test_Coding;
 
 public class Variable_Declaration_Initialization {
     public static void main(String[] args) {
-        int i=25;
-        double d = 3.14;
-        boolean b = true;
-        char c = 'A';
-        System.out.print("Integer: "+i);
-        System.out.print(" Double: "+d);
-        System.out.print(" Boolean: "+b);
-        System.out.print(" Character: "+c);
+        int i=25; //Declaration and initialization of Integer
+        double d = 3.14; //Declaration and initialization of Double
+        boolean b = true; //Declaration and initialization of boolean
+        char c = 'A';  //Declaration and initialization of Character
+        System.out.print("Integer: "+i); //Prints the integer value without new line
+        System.out.print(" Double: "+d); //Prints the Double value without new line
+        System.out.print(" Boolean: "+b); //Prints the Boolean value without new line
+        System.out.print(" Character: "+c); //Prints the Character value without new line
     }
 }
