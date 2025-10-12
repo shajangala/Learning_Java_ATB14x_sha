d777387ae6103609c2017343ad9999677b0d2bb2
Shamili <sha.jangala@gmail.com>
2025-10-12 12:05:37 +0530

    Test programs

--- src/test_Coding/Increment_Decrement_Operator.java
+++ src/test_Coding/Increment_Decrement_Operator.java
@@ -4,10 +4,10 @@ public class Increment_Decrement_Operator {
     public static void main(String[] args) {
         int i = 5;
         System.out.println("Original: "+i); //prints the original initialised value
-        System.out.println("Pre-Increment: "+ ++i ); //first increments the i value then prints
-        System.out.println("Post-Increment: "+ i++ + "( returned 6)"); //first prints the i value then increments
+        System.out.println("Pre-Increment: "+ ++i ); //first increments the i value then prints i becomes 6
+        System.out.println("Post-Increment: "+ i++ + "( returned 6)"); //first prints the i value then increments i becomes 7
         i = 6; // Again i value is initialised for the desired output
-        System.out.println("pre-Decrement: "+ --i + "( returned 5)"); //first decrements the i value then prints i value
-        System.out.println("Post-Decrement: "+ i-- + "( returned 5)"); //first prints the i value then decrements
+        System.out.println("pre-Decrement: "+ --i + "( returned 5)"); //first decrements the i value then prints i value i becomes 5
+        System.out.println("Post-Decrement: "+ i-- + "( returned 5)"); //first prints the i value then decrements after printing i becomes 4
     }
 }
